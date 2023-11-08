251a922e918dd8887ea9b49f3ced43066e43684f
Quest037 <149900263+Quest037@users.noreply.github.com>
2023-11-08 17:00:07 +0000

    Update page.tsx
    - Changed title
    - Removed description

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,9 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <>
-        <a href="http://localhost:3000" className="flex items-center auth-logo">
+        <a href="/" className="flex items-center auth-logo">
             <Image width={60} height={36} src="/assets/images/logo.png" className="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
-            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Backstory</span>
+            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">BackStory</span>
         </a>
 
         <div className="flex flex-col items-center md:flex-row md:h-screen justify-center">
@@ -16,10 +16,7 @@ export default function Home() {
             <div className="flex flex-col items-center justify-center w-full md:w-1/4">
                 <div className="w-full max-w-md space-y-8">
                     <div>
-                        <h1 className="text-2xl font-bold">Welcome back!</h1>
-                        <p className="mt-2 text-gray-600">
-                        Please sign in to your account.
-                        </p>
+                        <h1 className="text-2xl font-bold">To Enter This World</h1>
                     </div>
                         
                       <div>
@@ -27,7 +24,7 @@ export default function Home() {
                             type="submit"
                             className="w-full px-4 py-3 font-bold text-white bg-indigo-500 rounded-md hover:bg-indigo-600 focus:outline-none focus:shadow-outline-indigo focus:border-indigo-700"
                         >
-                          Login with Backstory
+                          Login with BackStory
                         </button>
                       </div>
                 </div>
