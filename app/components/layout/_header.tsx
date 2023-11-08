6cab440d1920f11ceebf5fc12a0c8f19f31dfcbf
Quest037 <149900263+Quest037@users.noreply.github.com>
2023-11-08 19:21:38 +0000

    Update _header.tsx
    Update header

--- app/components/layout/_header.tsx
+++ app/components/layout/_header.tsx
@@ -14,7 +14,7 @@ export default function Header({isLoggedIn}: HeaderProps) {
             <div className="flex flex-wrap justify-between items-center mx-auto">
                 <a href="dashboard" className="flex items-center">
                     <Image width={60} height={36} src="/assets/images/logo.png" className="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
-                    <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Backstory</span>
+                    <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Teloria</span>
                 </a>
                 <div className={"flex items-center lg:order-2 " + (isLoggedIn?"hidden":"block")}>
                     <a href="/login" className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Log in</a>
