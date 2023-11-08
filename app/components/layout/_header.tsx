289800bd614ed23b05b9f1560d8685a299047b9c
Quest037 <149900263+Quest037@users.noreply.github.com>
2023-11-08 18:49:32 +0000

    Update _header.tsx
    collections link

--- app/components/layout/_header.tsx
+++ app/components/layout/_header.tsx
@@ -12,7 +12,7 @@ export default function Header({isLoggedIn}: HeaderProps) {
       <header>
         <nav className="z-10 bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800 w-full fixed">
             <div className="flex flex-wrap justify-between items-center mx-auto">
-                <a href="http://localhost:3000" className="flex items-center">
+                <a href="dashboard" className="flex items-center">
                     <Image width={60} height={36} src="/assets/images/logo.png" className="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
                     <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Backstory</span>
                 </a>
@@ -27,7 +27,9 @@ export default function Header({isLoggedIn}: HeaderProps) {
                 </div>
 
                 <div className={"flex items-center lg:order-2 " + (isLoggedIn?"block":"hidden")}>
-                    <Image src="/assets/images/header/collections.png" alt="Collection" width={30} height={30} className="mr-4" />
+                    <a href="collections" className="flex items-center">
+                        <Image src="/assets/images/header/collections.png" alt="Collection" width={30} height={30} className="mr-4" />
+                    </a>
                     <Image src="/assets/images/account/user.jpg" className="rounded-full border-2" alt="User avatar" width={40} height={40} style={{objectFit:'cover', height:'40px'}} />
                 </div>
             </div>
