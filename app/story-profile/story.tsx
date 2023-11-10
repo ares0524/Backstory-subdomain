44cc1c30d518d04da264c7eb06049e37497f747f
Quest037 <149900263+Quest037@users.noreply.github.com>
2023-11-10 18:45:32 +0000

    Update story.tsx
    Redesigned story profile page

--- app/story-profile/story.tsx
+++ app/story-profile/story.tsx
@@ -71,7 +71,7 @@ export default function Story() {
                     <button className="text-white w-full mb-4 bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 focus:outline-none">Add Chapter</button>
                 </div>
 
-                <div className="w-5/6">
+                <div className="w-4/6">
                   <div className="my-2 ml-6 space-y-1 list-decimal list-inside p-4 min-h-[800px]">
                       <div className="relative z-0 w-full mb-3 group">
                           <input type="text" name="chapter-title" id="chapter-title" className="block py-2.5 px-0 w-1/2 text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
@@ -80,6 +80,10 @@ export default function Story() {
                       <textarea placeholder="Chapter description here..." cols={3} className="w-1/2 mb-3 text-sm bg-[#2f2f2f] border-[1px] p-1 rounded-md"></textarea>
                       <Quill modules={{ toolbar:['bold', 'underline', 'italic']}} formats={formats} theme="bubble"></Quill>
                   </div>
+                </div>
+
+                <div className="w-1/6">
+                  
                 </div>
             </div>
         </>
