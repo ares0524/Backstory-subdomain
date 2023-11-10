fc37d79edd1a3a5a70dd7ff4a8879c523faa1005
Quest037 <149900263+Quest037@users.noreply.github.com>
2023-11-10 16:17:48 +0000

    Update page.tsx

--- app/collection-profile/page.tsx
+++ app/collection-profile/page.tsx
@@ -38,7 +38,7 @@ export default function CollectionProfile() {
                                 <svg className="w-3 h-3 text-gray-400 mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 9 4-4-4-4"/>
                                 </svg>
-                                <a href="#" className="ml-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ml-2 dark:text-gray-400 dark:hover:text-white">Collection Name</a>
+                                <a href="#" className="ml-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ml-2 dark:text-gray-400 dark:hover:text-white">Collection</a>
                             </div>
                         </li>
                     </ol>
