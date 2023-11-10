168a82b0515e8f750bb39e1c217bc619f5ea1e1e
Quest037 <149900263+Quest037@users.noreply.github.com>
2023-11-10 17:19:26 +0000

    Update stats.tsx
    Added padding

--- app/story-profile/stats.tsx
+++ app/story-profile/stats.tsx
@@ -3,25 +3,25 @@ export default function Stats() {
         <>
             <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
                 <div className="grid grid-cols-2 row-gap-8 md:grid-cols-3 py-4 border rounded-lg">
-                    <div className="text-center md:border-r">
+                    <div className="text-center md:border-r pb-3">
                         <p className="text-sm font-medium tracking-widest text-gray-400 lg:text-base">
                             Word Count
                         </p>
                         <h6 className="text-4xl font-bold lg:text-5xl xl:text-6xl">86K</h6>
                     </div>
-                    <div className="text-center md:border-r">
+                    <div className="text-center md:border-r pb-3">
                         <p className="text-sm font-medium tracking-widest text-gray-400 lg:text-base">
                             Chapter Count
                         </p>
                         <h6 className="text-4xl font-bold lg:text-5xl xl:text-6xl">12</h6>
                     </div>
-                    <div className="text-center">
+                    <div className="text-center pb-3">
                         <p className="text-sm font-medium tracking-widest text-gray-400 lg:text-base">
                             Words per Chapter
                         </p>
                         <h6 className="text-4xl font-bold lg:text-5xl xl:text-6xl">7.2K</h6>
                     </div>
-                    <div className="text-center md:border-r">
+                    <div className="text-center md:border-r pb-3">
                         <p className="text-sm font-medium tracking-widest text-gray-400 lg:text-base">
                             Longest Chapter
                         </p>
@@ -30,7 +30,7 @@ export default function Stats() {
                             Chapter 4
                         </p>
                     </div>
-                    <div className="text-center md:border-r">
+                    <div className="text-center md:border-r pb-3">
                         <p className="text-sm font-medium tracking-widest text-gray-400 lg:text-base">
                             Shortest Chapter
                         </p>
