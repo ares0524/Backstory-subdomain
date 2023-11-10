0a06beb09e47ae36fa2d2e799801f04160acda71
Quest037 <149900263+Quest037@users.noreply.github.com>
2023-11-10 18:10:39 +0000

    Update detail.tsx
    text-white

--- app/story-profile/detail.tsx
+++ app/story-profile/detail.tsx
@@ -9,8 +9,8 @@ export default function Detail() {
             </div>
             <div className="w-1/4 m-auto">
                 <div className="px-4 sm:px-0">
-                    <h3 className="text-base font-semibold leading-7 text-white">Story Information</h3>
-                    <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-500">Story details and application.</p>
+                    <h3 className="text-base font-semibold leading-7 text-white">Story Details</h3>
+                    <p className="mt-1 max-w-2xl text-sm leading-6 text-white">Info about this story</p>
                 </div>
                 <div className="mt-6 border-t border-gray-100">
                     <dl className="divide-y divide-gray-100">
@@ -24,15 +24,15 @@ export default function Detail() {
                         </div>
                         <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
                             <dt className="text-sm font-medium leading-6 text-white">Type</dt>
-                            <dd className="mt-1 text-sm leading-6 text-gray-300 sm:col-span-2 sm:mt-0">Novelette</dd>
+                            <dd className="mt-1 text-sm leading-6 text-gray-300 sm:col-span-2 sm:mt-0">Novel</dd>
                         </div>
                         <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
                             <dt className="text-sm font-medium leading-6 text-white">Word Count</dt>
-                            <dd className="mt-1 text-sm leading-6 text-gray-300 sm:col-span-2 sm:mt-0">20000</dd>
+                            <dd className="mt-1 text-sm leading-6 text-gray-300 sm:col-span-2 sm:mt-0">86k</dd>
                         </div>
                         <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
                             <dt className="text-sm font-medium leading-6 text-white">Published Date</dt>
-                            <dd className="mt-1 text-sm leading-6 text-gray-300 sm:col-span-2 sm:mt-0">15.10.2023</dd>
+                            <dd className="mt-1 text-sm leading-6 text-gray-300 sm:col-span-2 sm:mt-0">--</dd>
                         </div>
                     </dl>
                 </div>
