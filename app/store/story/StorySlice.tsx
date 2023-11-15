1a8a7f7841649224b077529109e5cacc0482edbf
ares0524 <snow520scene@gmail.com>
2023-11-15 12:37:42 +0900

    set session in backend for auth

--- app/store/story/StorySlice.tsx
+++ app/store/story/StorySlice.tsx
@@ -27,27 +27,30 @@ export const addStory = (data: any) =>async (dispatch: AppDispatch) => {
     console.log(data);
 
     axios.defaults.withCredentials = true;
-    axios.post(process.env.SERVER_API + '/story/add/', data)
-        .then((res) => {
-            
-            if (res.data.status == 'fail') {
-                console.log(res.data);
-                toast(res.data.msg, {
-                    autoClose: 2000,
-                    type: 'warning',
-                    theme: 'dark'
-                })
-            } else {
-                toast(res.data.msg, {
-                    autoClose: 2000,
-                    type: 'success',
-                    theme: 'dark'
-                })
-            }
-        })
-        .catch(err => {
-            console.log("error => ", err);
-        })
+    axios.post(process.env.SERVER_API + '/story/add/', data, {
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        withCredentials: true
+    }).then((res) => {
+        if (res.data.status == 'fail') {
+            console.log(res.data);
+            toast(res.data.msg, {
+                autoClose: 2000,
+                type: 'warning',
+                theme: 'dark'
+            })
+        } else {
+            toast(res.data.msg, {
+                autoClose: 2000,
+                type: 'success',
+                theme: 'dark'
+            })
+        }
+    })
+    .catch(err => {
+        console.log("error => ", err);
+    })
 }
 
 export default StorySlice.reducer;
